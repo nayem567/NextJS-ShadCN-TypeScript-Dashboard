ad4551b3518db19881a0501d2cc24f331d090d7c
nayem567 <inew2732@gmail.com>
2025-05-21 16:36:49 +0600

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import { CirclePlus } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button className="rounded-full text-custom-color" size={"xl"}>
-        <CirclePlus />
-        Click Me
-      </Button>
+    <div className="">
+      
     </div>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,52 @@
+import { Link, LogOut, Moon, Settings, User } from "lucide-react";
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* LEFT side */}
+      collapseButton
+      {/* RIGHT side */}
+      <div className="flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+        <Moon />
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
